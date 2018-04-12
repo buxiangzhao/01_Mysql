28989ec42f7d89d47ae77edc861431c7041eeb35
buxiangzhao <1016862070@qq.com>
2018-04-12 15:02:32 +0800

    集成邮件发送

---
+++ src/test/java/com/bxz/Tset.java
@@ -0,0 +1,27 @@
+package com.bxz;
+
+import com.bxz.enums.MailContentTypeEnum;
+import com.bxz.utils.MailSender;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by SQ_BXZ on 2018-04-12.
+ */
+public class Tset {
+
+	@Test
+	public void sendMail() throws Exception {
+		new MailSender().content("发送内容")
+				.contentType(MailContentTypeEnum.TEXT)
+				.title("发送测试")
+				.targets(new ArrayList<String>() {
+					{
+						add("1647623919@qq.com");
+					}
+				})
+				.send();
+		System.out.println("ddddddd");
+	}
+}
